cd31eb5373baa352e77fac055169268bf143d9f7
Cytech <22370499+DevCytech@users.noreply.github.com>
2020-12-10 10:56:41 -0500

    Created client file.
    Created the basic client building file.

---
+++ src/bot.js
@@ -0,0 +1,16 @@
+// Setup the client
+const { Client } = require('discord.js');
+const client = new Client();
+
+// Setup Values
+const { PRODUCTION, TOKEN } = process.env;
+client.isProduction = PRODUCTION;
+
+// Log the client in
+client.login(TOKEN);
+
+// Export the client
+module.exports.client = client;
+
+// Startup the bot
+// require('./manager/ready')
