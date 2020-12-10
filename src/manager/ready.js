b3c2497c013d4b21e2c9f78e2d1c7c24096727b3
Cytech <22370499+DevCytech@users.noreply.github.com>
2020-12-10 11:10:25 -0500

    Created startup management file
    This file is to start the bot loading all of its required files. This allows the bot to be able to be logged in and ready before feeding it data. Just framework notes and stuff nothing fun yet...

---
+++ src/manager/ready.js
@@ -0,0 +1,10 @@
+// Variables
+const { client } = require('../bot');
+
+// When the client is logged in
+client.on('ready', async () => {
+	// Load the databse
+	// Load the commands
+	// Load the events
+	// Setup extra client values
+});
