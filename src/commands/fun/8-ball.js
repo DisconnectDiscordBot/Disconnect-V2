acec254cd976277bc4e034b3424b375d9f867d45
Cytech <22370499+CytechYT@users.noreply.github.com>
2020-12-31 01:53:09 -0500

    8 Ball says it's a definite

---
+++ src/commands/fun/8-ball.js
@@ -0,0 +1,29 @@
+const {
+	'eight-ball': responses,
+} = require('../../../assets/responses/text.json');
+const { createEmbed, improperUsage } = require('../../utils/embed');
+
+module.exports.run = async ({ message, args }) => {
+	if (!args[1])
+		return message.channel.send(
+			improperUsage(
+				"The magical 8-ball doesn't know how to respond. Please provide it a question you would like answered.",
+			),
+		);
+	else {
+		const e = await createEmbed({
+			author: {
+				username: 'The 8-ball says...',
+				avatarURL:
+					'https://images-ext-1.discordapp.net/external/KI1lAF4bXFR4XdGZ0asU2nt5WhpvUGBf2hwIf5wewZo/https/cdn.discordapp.com/emojis/793172531938459648.png',
+			},
+			body: responses[Math.floor(Math.random() * responses.length)],
+		});
+		return message.channel.send(e);
+	}
+};
+
+module.exports.config = {
+	name: '8-ball',
+	aliases: ['super-8-ball'],
+};
