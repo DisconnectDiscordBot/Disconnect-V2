176cb35db56f1b77ffb9976ece21356ca7d56c57
Cytech <22370499+CytechYT@users.noreply.github.com>
2021-01-02 02:37:46 -0500

    New Time Formats
    Updated old broken time formatters has room for inprobements later on

--- src/tools.js
+++ src/tools.js
@@ -1,3 +1,6 @@
+const moment = require('moment');
+require('moment-duration-format');
+
 // Get Member
 module.exports.fetchMember = (message, find = '') => {
 	find = find.toLowerCase();
@@ -18,4 +21,40 @@ module.exports.fetchMember = (message, find = '') => {
 
 	if (!result) return null;
 	return result;
+};
+
+// Time Since
+module.exports.formatDate = (date) => {
+	return moment.utc(date).format('ddd, DD MMM YYYY');
+};
+
+module.exports.formatTimeSince = (time) => {
+	return moment
+		.duration(time)
+		.format('d [days], h [hours], m [minutes], [and] s [seconds]', {
+			trim: 'small',
+		});
+};
+
+// Date Since
+module.exports.getTimeSince = function (date) {
+	const years = moment().diff(date, 'years', false);
+	const months = moment().diff(date, 'months', false) - 12 * years;
+	const days = moment().diff(date, 'days', false) - 365 * years - 30 * months;
+
+	if (years !== 0)
+		return months !== 0
+			? `${years} ${years !== 1 ? 'years' : 'year'} and ${months} ${
+					months !== 1 ? 'months' : 'month'
+			  } ago.`
+			: `${years} ${years !== 1 ? 'years' : 'year'} ago.`;
+
+	if (months !== 0)
+		return days !== 0
+			? `${months} ${months !== 1 ? 'months' : 'month'} and ${days} ${
+					days !== 1 ? 'days' : 'day'
+			  } ago.`
+			: `${months} ${months !== 1 ? 'months' : 'month'} ago.`;
+
+	return `${days} ${days !== 0 ? 'days' : 'day'} ago.`;
 };
