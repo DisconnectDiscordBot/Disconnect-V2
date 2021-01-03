31f57426b35be0a855686dde5ca210df0702dbf3
Cytech <22370499+CytechYT@users.noreply.github.com>
2021-01-03 15:26:49 -0500

    Some More fun commands
    Now can get advice or fun facts, try your luck at slots, russian roulette or rock paper scissors, and if your looking for some funnies look at memes.

---
+++ src/commands/fun/meme.js
@@ -0,0 +1,61 @@
+const agent = require('superagent');
+const { createEmbed } = require('../../utils/embed');
+const subReddits = [
+	'meme',
+	'animemes',
+	'MemesOfAnime',
+	'animememes',
+	'AnimeFunny',
+	'dankmemes',
+	'dankmeme',
+	'wholesomememes',
+	'MemeEconomy',
+	'techsupportanimals',
+	'meirl',
+	'me_irl',
+	'2meirl4meirl',
+	'AdviceAnimals',
+];
+
+async function getFromReddit() {
+	const reddit = subReddits[Math.floor(Math.random() * subReddits.length)];
+	console.log(reddit);
+
+	const { body } = await agent
+		.get(`https://www.reddit.com/r/${reddit}.json?sort=top&t=week`)
+		.query({ limit: 800 });
+
+	const post =
+		body.data.children[
+			Math.floor(Math.random() * body.data.children.length)
+		];
+
+	if (
+		(!post.data.url.endsWith('.png') &&
+			!post.data.url.endsWith('.jpg') &&
+			!post.data.url.endsWith('.jpeg')) ||
+		post.is_video
+	)
+		return await getFromReddit();
+	else return post;
+}
+
+module.exports.run = async ({ message }) => {
+	const result = await getFromReddit();
+
+	const title = result.data.title;
+	const image = result.data.url;
+
+	return message.channel.send(
+		createEmbed({
+			title,
+			image,
+			footer: `Subreddit: ${result.data.subreddit_name_prefixed}`,
+		}),
+	);
+};
+
+module.exports.config = {
+	name: 'meme',
+	aliases: ['random-meme'],
+};
---
+++ src/commands/fun/rock-paper-scissors.js
@@ -0,0 +1,69 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports.run = async ({ client, message, args }) => {
+	const symbols = [
+		client.emojis.cache.get('795378188875464715'),
+		':paper:',
+		':scissors:',
+	];
+
+	const translateSymbols = {
+		rock: client.emojis.cache.get('795378188875464715'),
+		paper: ':paper:',
+		scissors: ':scissors:',
+	};
+
+	const filter = (reaction, user) => {
+		return (
+			(reaction.emoji === symbols[0] && user.id === message.author.id) ||
+			(reaction.emoji.name === '📄' && user.id === message.author.id) ||
+			(reaction.emoji.name === '✂' && user.id === message.author.id)
+		);
+	};
+
+	const choice = args.join(' ').toLowerCase();
+	const rpsChoice = symbols[Math.floor(Math.random() * 3)];
+	console.log(rpsChoice);
+
+	const e = new MessageEmbed().setTitle('Rock Paper Scissors');
+
+	async function missingRPS() {
+		e.setDescription('Please select your pick below.');
+		const msg = await message.channel.send(e);
+		await msg.react(symbols[0]);
+		await msg.react('📄');
+		await msg.react('✂');
+
+		const collector = msg.createReactionCollector(filter, {
+			time: 60000,
+		});
+
+		collector.on('collect', async (reaction, user) => {
+			await msg.reactions.removeAll();
+			e.setDescription(
+				`I pick ${rpsChoice}! You picked: ${reaction.emoji}`,
+			);
+			return msg.edit(e);
+		});
+
+		collector.on('end', () => {});
+	}
+
+	if (choice) {
+		if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
+			return await missingRPS();
+		} else {
+			e.setDescription(
+				`I pick ${rpsChoice}! You picked: ${translateSymbols[choice]}`,
+			);
+			return message.channel.send(e);
+		}
+	} else {
+		return await missingRPS();
+	}
+};
+
+module.exports.config = {
+	name: 'rock-paper-scissors',
+	aliases: ['r-p-s'],
+};
