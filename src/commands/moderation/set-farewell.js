307f90a9b5c21e65bd97e21b4c183e08b1d2a224
Cytech <22370499+CytechYT@users.noreply.github.com>
2021-03-01 20:53:31 -0500

    Welcoming + Farewell
    Disconnect will not somewhat welcome people to guilds.

---
+++ src/commands/moderation/set-farewell.js
@@ -0,0 +1,139 @@
+const {
+	improperUsage,
+	createSuccessEmbed,
+	premiumOnly,
+} = require('../../utils/embed');
+const { MessageEmbed } = require('discord.js');
+const palette = require('../../../assets/colors.json');
+const config = require('../../../assets/config.json');
+
+module.exports.run = async ({ message, args, guildData }) => {
+	const e = new MessageEmbed()
+		.setTitle('Farewell Messages')
+		.setColor(palette.primary);
+
+	switch (args[0] ? args[0].toLowerCase() : null) {
+		case 'enable':
+			if (guildData.farewellEnabled) {
+				return message.channel.send(
+					improperUsage('Farewell is already enabled!'),
+				);
+			}
+
+			if (!guildData.farewellChannel.length) {
+				return message.channel.send(
+					improperUsage(
+						`Please set a farewell channel before enabling farewell messages. Example ${
+							guildData.prefix ? guildData.prefix : config.prefix
+						}farewell channel #welcome`,
+					),
+				);
+			}
+
+			guildData.farewellEnabled = true;
+			await guildData.save();
+			return message.channel.send(
+				createSuccessEmbed(
+					`Farewell messages has now been **Enabled** in <#${guildData.farewellChannel}>!`,
+				),
+			);
+
+		case 'disable':
+			if (!guildData.farewellEnabled) {
+				return message.channel.send(
+					improperUsage('Farewell is already disabled!'),
+				);
+			}
+
+			guildData.farewellEnabled = false;
+			await guildData.save();
+			return message.channel.send(
+				createSuccessEmbed(
+					`Farewell messages has now been **Disabled** in <#${guildData.farewellChannel}>!`,
+				),
+			);
+
+		case 'channel':
+			if (!args[1]) {
+				return message.channel.send(
+					improperUsage(
+						'Please specify the channel you would like to send the welcome messages.',
+					),
+				);
+			}
+
+			const channel =
+				message.guild.channels.cache.get(
+					args
+						.slice(1)
+						.join(' ')
+						.replace(/>/g, '')
+						.replace(/@/g, '')
+						.replace(/</g, '')
+						.replace(/#/g, ''),
+				) ||
+				message.guild.channels.cache.find(
+					(c) => c.name == args.slice(1).join(' '),
+				);
+			if (!channel) {
+				return message.channel.send(
+					improperUsage(
+						'I was unable to find the channel you were looking for.',
+					),
+				);
+			}
+
+			guildData.farewellChannel = channel.id;
+			await guildData.save();
+			return message.channel.send(
+				createSuccessEmbed(
+					`Farewell channel has now been set to <#${guildData.farewellChannel}>!`,
+				),
+			);
+
+		case 'format':
+			if (!args[1]) {
+				return message.channel.send(
+					improperUsage(
+						'Please specify weather you would like to set the welcome message to be an `image` or `text`.',
+					),
+				);
+			}
+
+			if (args[1].toLowerCase() === 'image') {
+				guildData.farewellFormat = 'image';
+				await guildData.save();
+				return message.channel.send(
+					createSuccessEmbed(
+						'Farewell messages will now send **images** instead of text on join.',
+					),
+				);
+			} else {
+				guildData.farewellFormat = 'text';
+				await guildData.save();
+				return message.channel.send(
+					createSuccessEmbed(
+						'Farewell messages will now send **text** instead of an image on join.',
+					),
+				);
+			}
+
+		default:
+		case 'help':
+			e.setDescription(
+				'Hey, I can send off your members for you too!\nIf you would like to setup farewell messages please use one of the following options.\n\n`enable | disable` - Enable or disable farewell messages.\n`channel` - Change the channel farewell messages are being sent to.\n`format` - Change wether farewell is text or an image. (*More customization coming soon!*)',
+			).setFooter(
+				`I also do welcoming messages, check them out with ${
+					guildData.prefix ? guildData.prefix : config.prefix
+				}farewell`,
+			);
+
+			return message.channel.send(e);
+	}
+};
+
+module.exports.config = {
+	name: 'farewell',
+	aliases: ['set-farewell'],
+	permissions: ['MANAGE_GUILD'],
+};
---
+++ src/events/discord/guildMemberAdd.js
@@ -0,0 +1,90 @@
+const { client } = require('../../bot');
+const { createCanvas, loadImage } = require('canvas');
+const { MessageAttachment, Message } = require('discord.js');
+const { formatOrdinal, getJoinPosition } = require('../../tools');
+const { get: getGuild } = require('../../models/guilds');
+
+// Manage Text
+const applyText = (canvas, text) => {
+	const ctx = canvas.getContext('2d');
+	let fontSize = 56;
+
+	// Make sure the text doesn't overflow off the canvas;
+	do {
+		ctx.font = `${(fontSize -= 10)}px sans-serif`;
+	} while (ctx.measureText(text).width > canvas.width - 300);
+
+	return ctx.font;
+};
+
+client.on('guildMemberAdd', async (member) => {
+	if (!member) return;
+
+	const guild = member.guild;
+	const guildData = await getGuild(guild);
+
+	if (!guildData || !guildData.welcomingEnabled) return;
+	if (!guildData.welcomingChannel) return;
+	const channel = client.channels.cache.get(guildData.welcomingChannel);
+
+	// Check to make sure the bot can speak
+	if (!channel.permissionsFor(client.user).has('SEND_MESSAGES')) return;
+
+	// Check format
+	if (!guildData.welcomingFormat || guildData.welcomingFormat === 'image') {
+		// Create our canvas
+		const canvas = createCanvas(1000, 333);
+		const ctx = canvas.getContext('2d');
+
+		// Add background
+		const backgroundImage = await loadImage(
+			'./assets/images/background.png',
+		);
+		ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+		// Welcome User
+		ctx.fillStyle = '#ffffff';
+		ctx.font = applyText(canvas, `Welcome, ${member.displayName}`);
+		ctx.fillText(
+			`Welcome, ${member.displayName}`,
+			canvas.width / 3,
+			canvas.height / 2.4,
+		);
+
+		// What number member they are
+		const guildText = `to ${guild.name}!!`;
+		ctx.font = applyText(canvas, guildText);
+		ctx.fillText(guildText, canvas.width / 3, canvas.height / 1.6);
+
+		// Add their avatar
+		ctx.arc(170, 160, 120, 0, Math.PI * 2, true);
+		ctx.lineWidth = 6;
+		ctx.strokeStyle = '#FFFFFF';
+		ctx.stroke();
+		ctx.closePath();
+		ctx.clip();
+		const avatar = await loadImage(
+			member.user
+				.displayAvatarURL({ dynamic: true })
+				.replace('.webp', '.jpg') + '?size=1024',
+		);
+		ctx.drawImage(avatar, 40, 40, 250, 250);
+
+		// Add the new image as a attachment
+		const attachment = new MessageAttachment(
+			canvas.toBuffer(),
+			'welcome.png',
+		);
+
+		// Send message
+		channel.send(attachment);
+	} else {
+		return channel.send(
+			`Welcome, ${member.displayName} to **${
+				guild.name
+			}**. You are the ${formatOrdinal(
+				getJoinPosition(member.id, guild) + 1,
+			)} member!`,
+		);
+	}
+});
