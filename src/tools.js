fee79de3a940538486940c565e5660cdd7f216d2
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-03-16 11:03:53 -0400

    Attempting to fix complexity

--- src/tools.js
+++ src/tools.js
@@ -56,23 +56,24 @@ module.exports.getTimeSince = function (date) {
 	const years = moment().diff(date, 'years', false);
 	const months = moment().diff(date, 'months', false) - 12 * years;
 	const days = moment().diff(date, 'days', false) - 365 * years - 30 * months;
+	let timesince = ''; 
 
-	if (years !== 0)
-		return months !== 0
-			? `${years} ${years !== 1 ? 'years' : 'year'} and ${months} ${
-					months !== 1 ? 'months' : 'month'
-			  } ago`
+	if (years !== 0) {
+		timesince = months !== 0
+			? `${years} ${years !== 1 ? 'years' : 'year'} and ${months} ${months !== 1 ? 'months' : 'month' } ago`
 			: `${years} ${years !== 1 ? 'years' : 'year'} ago`;
-
-	if (months !== 0)
-		return days !== 0
-			? `${months} ${months !== 1 ? 'months' : 'month'} and ${days} ${
-					days !== 1 ? 'days' : 'day'
-			  } ago`
+	}
+	
+	if (months !== 0) {
+		timesince = days !== 0
+			? `${months} ${months !== 1 ? 'months' : 'month'} and ${days} ${days !== 1 ? 'days' : 'day'} ago`
 			: `${months} ${months !== 1 ? 'months' : 'month'} ago`;
-
+	}
+	
 	return `${
-		days == 0
+		timesince 
+			? timesince 
+			: days === 0
 			? 'less than a day'
 			: days !== 1
 			? `${days} days`
