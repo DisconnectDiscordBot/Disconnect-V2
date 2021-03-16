e19c454fb7e3bc84fdd5c249defb20d80a930f5a
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-03-16 14:20:54 -0400

    Updated roleplay commands to EMCA standards

--- src/commands/roleplay/poke.js
+++ src/commands/roleplay/poke.js
@@ -6,21 +6,15 @@ module.exports.run = async ({ message, args }) => {
 	// Get user
 	let user;
 	if (args[0]) user = fetchMember(message, args.join(' '));
-	if (!user && args[0]) user = args.join(' ');
 	if (user && typeof user !== 'string') user = user.displayName;
 
-	// Set the sentence
-	const title = user
-		? `${message.member.displayName} has poked ${user}!`
-		: `${message.member.displayName} is poking themself!`;
-
-	// Get Image
-	const image = images[Math.floor(Math.random() * images.length)];
-
+	// Send message
 	return message.channel.send(
 		createEmbed({
-			title,
-			image,
+			title: user
+				? `${message.member.displayName} has poked ${user}!`
+				: `${message.member.displayName} is poking themself!`,
+			image: images[Math.floor(Math.random() * images.length)],
 		}),
 	);
 };
--- src/commands/roleplay/tickle.js
+++ src/commands/roleplay/tickle.js
@@ -5,22 +5,20 @@ const { tickle: images } = require('../../../assets/links.json');
 module.exports.run = async ({ message, args }) => {
 	// Get user
 	let user;
-	if (args[0]) user = fetchMember(message, args.join(' '));
-	if (!user && args[0]) user = args.join(' ');
-	if (user && typeof user !== 'string') user = user.displayName;
+	if (args[0]) {
+		user = fetchMember(message, args.join(' '));
+	}
+	if (user && typeof user !== 'string') {
+		user = user.displayName;
+	}
 
-	// Set the sentence
-	const title = user
-		? `${message.member.displayName} has ticked ${user}!`
-		: `${message.member.displayName} is ticking themself!`;
-
-	// Get Image
-	const image = images[Math.floor(Math.random() * images.length)];
-
+	// Send message
 	return message.channel.send(
 		createEmbed({
-			title,
-			image,
+			title: user
+				? `${message.member.displayName} has ticked ${user}!`
+				: `${message.member.displayName} is ticking themself!`,
+			image: images[Math.floor(Math.random() * images.length)],
 		}),
 	);
 };
