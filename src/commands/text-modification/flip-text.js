d87d69635a395af0ab846577a90c547a53c0a778
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-03-16 15:28:19 -0400

    Reviewed Text Modification

--- src/commands/text-modification/flip-text.js
+++ src/commands/text-modification/flip-text.js
@@ -2,11 +2,11 @@ const { flip_text: flipText } = require('../../utils/translate');
 const { createEmbed, improperUsage } = require('../../utils/embed');
 
 module.exports.run = async ({ message, args }) => {
-	if (!args[0])
+	if (!args[0]) {
 		return message.channel.send(
 			improperUsage('Please provide text you would like to flip.'),
 		);
-	else {
+	} else {
 		return message.channel.send(
 			createEmbed({
 				title: 'I have flipped the text...',
