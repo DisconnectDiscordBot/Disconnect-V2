51aa90dbc590add2ac384885008eac06034deff7
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-04-07 09:20:52 -0400

    removed if (Q)

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -1,7 +1,6 @@
 const { musicEmbed } = require('../../utils/embed');
 
 module.exports.run = async ({ message, queue }) => {
-	if (Q)
 	queue.dispatcher.end();
 	return message.channel.send(
 		musicEmbed('Song Skipped', 'I have skipped the current song.'),
