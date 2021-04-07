f7f0edb2cb8b91cf48746f4f2e9b2041faa98925
Cytech <22370499+DevCytech@users.noreply.github.com>
2021-04-07 09:20:17 -0400

    isMusic not isPlaying

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -1,6 +1,7 @@
 const { musicEmbed } = require('../../utils/embed');
 
 module.exports.run = async ({ message, queue }) => {
+	if (Q)
 	queue.dispatcher.end();
 	return message.channel.send(
 		musicEmbed('Song Skipped', 'I have skipped the current song.'),
@@ -9,5 +10,5 @@ module.exports.run = async ({ message, queue }) => {
 
 module.exports.config = {
 	name: 'skip',
-	isPlaying: true,
+	isMusic: true,
 };
